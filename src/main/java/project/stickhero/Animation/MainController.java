c5898e2a1abcbaad6c139b7f572133b3fc6be5e0
Vijval9 <114911861+Vijval9@users.noreply.github.com>
2023-12-09 10:29:49 +0530

    Added music

--- src/main/java/project/stickhero/Animation/MainController.java
+++ src/main/java/project/stickhero/Animation/MainController.java
@@ -6,17 +6,42 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.File;
 import java.io.IOException;
+// music credit : Main Theme (Overture) | The Grand Score by Alexander Nakarada | https://creatorchords.com/
+//Music promoted by https://www.chosic.com/free-music/all/
+//Attribution 4.0 International (CC BY 4.0)
+//https://creativecommons.org/licenses/by/4.0/
+//
 
 public class MainController {
     @FXML
     private Button playButton;
 
 
+
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     public void initialize() {
+        //String path = "C:\\Users\\vijva\\Downloads\\Brendan_Kinsella_-_01_-_Bach_-_Aria_Variata_BWV_989_Variation_no1(chosic.com).mp3";
+        String path = "src/main/resources/Music/Theme.mp3";
+        Media med = new Media(new File(path).toURI().toString());
+        //edia med = new Media(new File())
+        MediaPlayer medp = new MediaPlayer(med);
+        medp.setOnEndOfMedia(new Runnable()
+        {
+            public void run()
+            {
+                medp.seek(Duration.ZERO);
+            }
+
+        });
+        medp.setAutoPlay(true);
         // Not Required Right now
     }
     @FXML
