73ef72c041b66d651e7b79c052fa4ab5e580917d
Vijval9 <114911861+Vijval9@users.noreply.github.com>
2023-12-10 01:23:42 +0530

    Update ProgressInfo.java

--- src/main/java/project/stickhero/UMLClasses/ProgressInfo.java
+++ src/main/java/project/stickhero/UMLClasses/ProgressInfo.java
@@ -56,3 +56,59 @@ public class ProgressInfo {
     }
 
 }
+
+/*
+
+package project.stickhero.UMLClasses;
+
+
+public class ProgressInfo {
+
+    
+    public ProgressInfo() {
+    }
+
+    
+    private int currentScore;
+
+    
+    private int totalCherries;
+
+    
+    private int highScore;
+
+    public void showProgress() {
+        // TODO implement here
+    }
+
+    
+    public int getCurrentScore() {
+        // TODO implement here
+        return 0;
+    }
+
+    
+    public int getHighScore() {
+        // TODO implement here
+        return 0;
+    }
+
+    
+    public int getTotalCherries() {
+        // TODO implement here
+        return 0;
+    }
+
+    public void setCurrentScore(int current) {
+        this.currentScore = current;
+    }
+
+    public void setTotalCherries(int totalCherries) {
+        this.totalCherries = totalCherries;
+    }
+
+    public void setHighScore(int highScore) {
+        this.highScore = highScore;
+    }
+}
+*/
